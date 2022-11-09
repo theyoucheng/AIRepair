2a02ccba109cbef25aa788f3a1b79686a6be0da3
HymnOfLight <30403502+HymnOfLight@users.noreply.github.com>
2022-11-09 14:38:03 +0000

    Add files via upload

---
+++ src/patch_apricot_weight_adjust.py
@@ -0,0 +1,315 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision import transforms
+from torchvision.utils import save_image
+from model.data_loader import CIFARVT
+import resnet as RN
+import tqdm
+import time
+import argparse
+from resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
+
+# create sample directory if not exists
+sample_dir = 'samples'
+if not os.path.exists(sample_dir):
+    os.makedirs(sample_dir)
+    
+# Hyper-parameters
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-err_src', type=int, default=-1)
+parser.add_argument('-err_dst', type=int, default=-1)
+parser.add_argument('--net_type', dest='nettype', default='resnet', type=str,
+                    help='networktype: resnet, mnist, and fmnist')
+parser.add_argument('--datasets', dest='datasets', default='imagenet', type=str,
+                    help='datasets (options: cifar10, cifar100, mnist, fmnist')
+parser.add_argument(
+    '--pretrained', dest= 'pretrained', default='CIFAR10_classifier_CNN2_iDLM_full', type=str)
+parser.add_argument(
+'--ticker', dest= 'ticker', default='/set/your/model/path', type=str, metavar='PATH')
+parser.add_argument('--depth', default=18, type=int,
+                    help='depth of the network (default: 18)')
+parser.add_argument(
+    '--trained', dest= 'trained', default='CIFAR10_classifier_CNN2_iDLM_full', type=str)
+args = parser.parse_args()
+
+img_size = 32
+h_dim = 800
+train_batch_size = 20
+test_batch_size = 256
+retrain_epoch_num = 5
+learning_rate = 2e-4
+ticker = args.ticker
+idlm_path = './weights/%s.pth' % ticker
+rdlm_dir = './rDLM_weights/'
+rdlm_query_str = 'CNN2'
+device = 'cuda'
+if args.err_src != -1 and args.err_dst != -1:
+    targeted = True
+    target_err_type = (args.err_src, args.err_dst)
+else:
+    targeted = False
+
+# if it is CIFAR10 dataset
+if args.datasets=="cifar10":
+    train_dataset = torchvision.datasets.CIFAR10(root='./',
+                                        train=True,
+                                        transform=transforms.ToTensor(),
+                                        download=True)
+    val_dataset = torchvision.datasets.CIFAR10(root='./',
+                                        train=False,
+                                        transform=transforms.ToTensor(),
+                                        download=True)
+    test_dataset = torchvision.datasets.CIFAR10(root='./',
+                                        train=False,
+                                        transform=transforms.ToTensor(),
+                                        download=True)
+elif args.datasets == 'cifar100':
+    train_dataset = torchvision.datasets.CIFAR100(root='./',
+                                    train=True,
+                                    transform=transforms.ToTensor(),
+                                    download=True)
+    val_dataset = torchvision.datasets.CIFAR100(root='./',
+                                        train=False,
+                                        transform=transforms.ToTensor(),
+                                        download=True)
+    test_dataset = torchvision.datasets.CIFAR100(root='./',
+                                        train=False,
+                                        transform=transforms.ToTensor(),
+                                        download=True)
+elif args.datasets == 'fmnist':
+    train_dataset = torchvision.datasets.FashionMNIST(root='./',
+                                        train=True,
+                                        transform=transforms.ToTensor(),
+                                        download=True)
+    val_dataset = torchvision.datasets.FashionMNIST(root='./',
+                                        train=False,
+                                        transform=transforms.ToTensor(),
+                                        download=True)
+    test_dataset = torchvision.datasets.FashionMNIST(root='./',
+                                        train=False,
+                                        transform=transforms.ToTensor(),
+                                        download=True)
+elif args.datasets == 'mnist':
+    train_dataset = torchvision.datasets.MNIST(root='./',
+                                        train=True,
+                                        transform=transforms.ToTensor(),
+                                        download=True)
+    val_dataset = torchvision.datasets.MNIST(root='./',
+                                        train=False,
+                                        transform=transforms.ToTensor(),
+                                        download=True)
+    test_dataset = torchvision.datasets.MNIST(root='./',
+                                        train=False,
+                                        transform=transforms.ToTensor(),
+                                        download=True)   
+                                                                                
+
+# Data loader
+train_data_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                                batch_size=train_batch_size, 
+                                                shuffle=True)
+val_data_loader = torch.utils.data.DataLoader(dataset=val_dataset,
+                                            batch_size=test_batch_size, 
+                                            shuffle=False)
+test_data_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                            batch_size=test_batch_size, 
+                                            shuffle=False)
+
+# neural net initialization
+if args.datasets == 'cifar10':
+    if args.depth == 18:
+        iDLM = ResNet18(10)
+    elif args.depth == 34:
+        iDLM = ResNet34(10)
+    elif args.depth == 50:
+        iDLM = ResNet50(10)
+    iDLM = torch.load(args.pretrained)
+    iDLM.to(device)
+    iDLM.eval()
+    optimizer = torch.optim.Adam(iDLM.parameters(), lr=learning_rate)
+    rDLMs = []
+    for filename in os.listdir(rdlm_dir):
+        if rdlm_query_str not in filename: continue
+        rDLM = torch.load(args.pretrained)
+        rDLM.to(device)
+        rDLM.eval()
+        rDLMs.append(rDLM)
+    loss_fn = nn.CrossEntropyLoss()
+elif args.datasets == 'cifar100':
+    if args.depth == 18:
+        iDLM = ResNet18(100)
+    elif args.depth == 34:
+        iDLM = ResNet34(100)
+    elif args.depth == 50:
+        iDLM = ResNet50(100)
+    iDLM.load_state_dict(torch.load(args.pretrained), strict=False)
+    iDLM.to(device)
+    iDLM.eval()
+    optimizer = torch.optim.Adam(iDLM.parameters(), lr=learning_rate)
+    rDLMs = []
+    for filename in os.listdir(rdlm_dir):
+        if rdlm_query_str not in filename: continue
+        rDLM = ResNet18(100)
+        rDLM.load_state_dict(torch.load(args.pretrained), strict=False)
+        rDLM.to(device)
+        rDLM.eval()
+        rDLMs.append(rDLM)
+    loss_fn = nn.CrossEntropyLoss()
+elif args.datasets == 'mnist' or 'fmnist':
+    iDLM = torch.load('RobustnessT_fmnist_baseline-Copy1.pt')
+    iDLM.to(device)
+    iDLM.eval()
+    optimizer = torch.optim.Adam(iDLM.parameters(), lr=learning_rate)
+    rDLMs = []
+    for filename in os.listdir(rdlm_dir):
+        if rdlm_query_str not in filename: continue
+        rDLM = torch.load('RobustnessT_fmnist_baseline-Copy1.pt')
+        rDLM.to(device)
+        rDLM.eval()
+        rDLMs.append(rDLM)
+    loss_fn = nn.CrossEntropyLoss()
+
+
+# utility function(s)
+def setWeights(model, weight_list):
+    for weight, (name, v) in zip(weight_list, model.named_parameters()):
+        attrs = name.split('.')
+        obj = model
+        for attr in attrs:
+            obj = getattr(obj, attr)
+        obj.data = weight
+
+def AdjustWeights(baseWeights, corrDiff, incorrDiff, a, b,
+                  strategy='both-org', lr=1e-3):
+    if 'org' in strategy:
+        sign = 1
+    else:
+        sign = -1
+    
+    p_corr, p_incorr = a/(a+b), b/(a+b)
+    
+    if 'both' in strategy:
+        return [b_w + sign*lr*(p_corr*cD - p_incorr*iD)
+                for b_w, cD, iD in zip(baseWeights, corrDiff, incorrDiff)]
+    elif 'corr' in strategy:
+        return [b_w + sign*lr*p_corr*cD
+                for b_w, cD in zip(baseWeights, corrDiff)]
+    elif 'incorr' in strategy:
+        return [b_w - sign*lr*p_incorr*iD
+                for b_w, iD in zip(baseWeights, incorrDiff)]
+    else:
+        raise ValueError(f'Unrecognized strategy {strategy}')
+
+def EvaluateModel(model, loader=val_data_loader):
+    with torch.no_grad():
+        total = 0.
+        correct = 0.
+        model.eval()
+        for tx, tx_class in loader:
+            tx = tx.to(device)
+            tclass_logits = model(tx)
+            _, mostprob_result = torch.max(tclass_logits, dim=1)
+            total += tx.size(0)
+            correct += torch.sum(mostprob_result == tx_class.to(device))
+    return correct.item()/total
+
+def TrainingModel(model, epoch_num=retrain_epoch_num):
+    model.train()
+    for epoch in tqdm.trange(epoch_num):
+        for i, (x, x_class) in enumerate(train_data_loader):
+            # Forward pass
+            x = x.to(device)
+            class_logits = model(x)
+
+            # Backprop and optimize
+            class_loss = loss_fn(class_logits, x_class.to(device))
+            loss = class_loss
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    model.eval()
+    
+
+# --- actual process --- #
+bestAcc = EvaluateModel(iDLM)
+with torch.no_grad():
+    best_weights = list(map(lambda x: x.data, iDLM.parameters()))
+
+t_acc = EvaluateModel(iDLM, loader=test_data_loader)
+print(f'org best acc (val): {bestAcc} | (tst): {t_acc}')
+last_improvement = 0
+
+s = time.perf_counter()
+print(f'Start time: {s}')
+
+for b_idx, (x, x_class) in enumerate(train_data_loader):
+    # Forward pass
+    with torch.no_grad():
+        x, x_class = x.to(device), x_class.to(device)
+        yOrigin = torch.argmax(iDLM(x), dim=1)
+        ySubList = [torch.argmax(rdlm(x), dim=1) for rdlm in rDLMs]
+
+        for i_idx in range(train_batch_size):
+            img_class = x_class[i_idx]
+            if yOrigin[i_idx] == img_class:
+                continue
+            elif targeted and not (img_class, yOrigin[i_idx]) == target_err_type:
+                continue
+            else:
+                correctSubModels = [rdlm for r_idx, rdlm in enumerate(rDLMs)
+                                    if ySubList[r_idx][i_idx] == img_class]
+                incorrectSubModels = [rdlm for r_idx, rdlm in enumerate(rDLMs)
+                                      if ySubList[r_idx][i_idx] != img_class]
+                if len(correctSubModels) == 0 or len(incorrectSubModels) == 0:
+                    continue # slightly different from paper
+                
+                correctWeightSum = [sum(t) for t in zip(*[m.parameters() for m in correctSubModels])]
+                correctWeights = [e/len(correctSubModels) for e in correctWeightSum]
+                incorrWeightSum = [sum(t) for t in zip(*[m.parameters() for m in incorrectSubModels])]
+                incorrWeights = [e/len(incorrectSubModels) for e in incorrWeightSum]
+                baseWeights = list(map(lambda x: x.data, iDLM.parameters()))
+                corrDiff = [b_w - c_w for b_w, c_w in zip(baseWeights, correctWeights)]
+                incorrDiff = [b_w - i_w for b_w, i_w in zip(baseWeights, incorrWeights)]
+                baseWeights = AdjustWeights(
+                    baseWeights, corrDiff, incorrDiff,
+                    len(correctSubModels), len(incorrectSubModels),
+                    strategy='both-org', lr=1e-3
+                )
+                setWeights(iDLM, baseWeights)
+    
+    currAcc = EvaluateModel(iDLM)
+    print(f'batch {b_idx} done, last improvement {b_idx-last_improvement} batches ago')
+    print(f'New accuracy prior training: {currAcc}')
+    if bestAcc < currAcc:
+        with torch.no_grad():
+            best_weights = list(map(lambda x: x.data, iDLM.parameters()))
+        
+        t_acc = EvaluateModel(iDLM, test_data_loader)
+        print(f'new best acc (val): {currAcc} | (tst): {t_acc}')
+        bestAcc = currAcc
+        last_improvement = b_idx
+    else:
+        if bestAcc != currAcc:
+            with torch.no_grad():
+                setWeights(iDLM, best_weights)
+        if last_improvement + 100 < b_idx:
+            print('No improvement for too long, terminating')
+            break
+    TrainingModel(iDLM)
+    currAcc = EvaluateModel(iDLM)
+    print(f'New accuracy post training: {currAcc}')
+
+e = time.perf_counter()
+print(f'End time: {e}')
+print(f'Total execution time: {e-s}')
+    
+#setWeights(iDLM, best_weights)
+#torch.save(iDLM.state_dict(), f'weights/%s_fixed_tgt%d%d.pth' % (ticker, target_err_type[0], target_err_type[1]))
+torch.save(iDLM, args.trained)
+
---
+++ src/patch_dl2_main.py
@@ -0,0 +1,324 @@
+import argparse
+import os
+import torch
+import numpy as np
+import torch.optim as optim
+import json
+import time
+from torchvision import datasets, transforms
+from oracles import DL2_Oracle
+from constraints import *
+from resnet import ResNet50
+from models import MLP, MnistNet
+from sklearn.decomposition import PCA
+import sys
+sys.path.append('../../')
+import dl2lib as dl2
+import time
+from vgg import VGG
+
+
+use_cuda = torch.cuda.is_available()
+
+
+def embed_batch(x_batch, pca):
+    n_batch = int(x_batch.size()[0])
+    n_feat = x_batch[0].numel()
+
+    big_batch = np.zeros((n_batch, n_feat))
+    for i in range(n_batch):
+        big_batch[i] = x_batch[i].cpu().view(-1).numpy()
+    embed_batch = pca.transform(big_batch)
+
+    x_batch = torch.from_numpy(embed_batch).float()
+    return x_batch
+
+
+def embed_pca(data, embed_dim):
+    n_data = len(data)
+    n_feat = torch.numel(data[0][0])
+
+    tot = n_data
+    ids = np.arange(n_data)
+    np.random.shuffle(ids)
+    ids = ids[:tot]
+    x_big = np.zeros((n_data, n_feat))
+    for i in range(tot):
+        x_big[i] = data[ids[i]][0].view(-1).cpu().numpy()
+    pca = PCA(n_components=embed_dim)
+    pca.fit(x_big)
+    return pca
+
+
+def train(args, oracle, net, device, train_loader, optimizer, epoch, pca=None):
+    t1 = time.time()
+    num_steps = 0
+    avg_train_acc, avg_constr_acc = 0, 0
+    avg_ce_loss, avg_dl2_loss = 0, 0
+    ce_loss = torch.nn.CrossEntropyLoss()
+
+    print('Epoch ', epoch)
+    for batch_idx, (data, target) in enumerate(train_loader):
+        x_batch, y_batch = data.to(device), target.to(device)
+        n_batch = int(x_batch.size()[0])
+        num_steps += 1
+
+        if pca is not None:
+            x_batch = embed_batch(x_batch, pca)
+            if use_cuda:
+                x_batch = x_batch.cuda()
+
+        x_outputs = net(x_batch)
+        x_correct = torch.mean(torch.argmax(x_outputs, dim=1).eq(y_batch).float())
+        ce_batch_loss = ce_loss(x_outputs, y_batch)
+        
+        if epoch <= args.delay or args.dl2_weight < 1e-7:
+            net.train()
+            optimizer.zero_grad()
+            ce_batch_loss.backward()
+            optimizer.step()
+
+            if batch_idx % args.print_freq == 0:
+                print('[%d] Train acc: %.4f' % (batch_idx, x_correct.item()))
+            continue
+
+        x_batches, y_batches = [], []
+        k = n_batch // oracle.constraint.n_tvars
+        assert n_batch % oracle.constraint.n_tvars == 0, 'Batch size must be divisible by number of train variables!'
+        for i in range(oracle.constraint.n_tvars):
+            x_batches.append(x_batch[i:(i + k)])
+            y_batches.append(y_batch[i:(i + k)])
+
+        net.eval()
+
+        if oracle.constraint.n_gvars > 0:
+            domains = oracle.constraint.get_domains(x_batches, y_batches)
+            z_batches = oracle.general_attack(x_batches, y_batches, domains, num_restarts=1, num_iters=args.num_iters, args=args)
+            _, dl2_batch_loss, constr_acc = oracle.evaluate(x_batches, y_batches, z_batches, args)
+        else:
+            _, dl2_batch_loss, constr_acc = oracle.evaluate(x_batches, y_batches, None, args)
+
+        avg_train_acc += x_correct.item()
+        avg_constr_acc += constr_acc.item()
+        avg_ce_loss += ce_batch_loss.item()
+        avg_dl2_loss += dl2_batch_loss.item()
+
+        if batch_idx % args.print_freq == 0:
+            print('[%d] Train acc: %.4f, Constr acc: %.4f, CE loss: %.3lf, DL2 loss: %.3lf' % (
+                batch_idx, x_correct.item(), constr_acc.item(), ce_batch_loss.item(), dl2_batch_loss.item()))
+        
+        net.train()
+        optimizer.zero_grad()
+        tot_batch_loss = args.dl2_weight * dl2_batch_loss + ce_batch_loss
+        tot_batch_loss.backward()
+        optimizer.step()
+    t2 = time.time()
+        
+    avg_train_acc /= float(num_steps)
+    avg_constr_acc /= float(num_steps)
+    avg_dl2_loss /= float(num_steps)
+    avg_ce_loss /= float(num_steps)
+    t = t2 - t1
+    torch.save(net, saved_model)
+    
+    return avg_train_acc, avg_constr_acc, avg_dl2_loss, avg_ce_loss, t
+
+
+def test(args, oracle, model, device, test_loader, pca=None):
+    loss = torch.nn.CrossEntropyLoss()
+    model.eval()
+    test_loss = 0
+    correct, constr, num_steps, pgd_ok = 0, 0, 0, 0
+    
+    for data, target in test_loader:
+        num_steps += 1
+        x_batch, y_batch = data.to(device), target.to(device)
+        n_batch = int(x_batch.size()[0])
+
+        if pca is not None:
+            x_batch = embed_batch(x_batch, pca)
+            if use_cuda:
+                x_batch = x_batch.cuda()
+
+        x_batches, y_batches = [], []
+        k = n_batch // oracle.constraint.n_tvars
+        assert n_batch % oracle.constraint.n_tvars == 0, 'Batch size must be divisible by number of train variables!'
+
+        for i in range(oracle.constraint.n_tvars):
+            x_batches.append(x_batch[i:(i + k)])
+            y_batches.append(y_batch[i:(i + k)])
+
+        if oracle.constraint.n_gvars > 0:
+            domains = oracle.constraint.get_domains(x_batches, y_batches)
+            z_batches = oracle.general_attack(x_batches, y_batches, domains, num_restarts=1, num_iters=args.num_iters, args=args)
+            _, dl2_batch_loss, constr_acc = oracle.evaluate(x_batches, y_batches, z_batches, args)
+        else:
+            _, dl2_batch_loss, constr_acc = oracle.evaluate(x_batches, y_batches, None, args)
+
+        output = model(x_batch)
+        test_loss += loss(output, y_batch).item()  # sum up batch loss
+        pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            
+        correct += pred.eq(y_batch.view_as(pred)).sum().item()
+        constr += constr_acc.item()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Pred. Accuracy: {}/{} ({:.0f}%)'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    print('Constr. acc: %.4f' % (constr / float(num_steps)))
+
+    return correct / len(test_loader.dataset), constr / float(num_steps)
+
+
+parser = argparse.ArgumentParser(description='Train NN with constraints.')
+parser = dl2.add_default_parser_args(parser)
+parser.add_argument('--saved_model', type = str, required=True, help = 'PATH to store the repaired models and the file name.' )
+parser.add_argument('--batch-size', type=int, default=128, help='Number of samples in a batch.')
+parser.add_argument('--num-iters', type=int, default=50, help='Number of oracle iterations.')
+parser.add_argument('--num-epochs', type=int, default=300, help='Number of epochs to train for.')
+parser.add_argument('--dl2-weight', type=float, default=0.0, help='Weight of DL2 loss.')
+parser.add_argument('--dataset', type=str, default='mnist', help='Dataset on which to train.')
+parser.add_argument('--pretrained', action='store_true', help='Whether to use pretrained model.')
+parser.add_argument('--embed', action='store_true', help='Whether to embed the points.')
+parser.add_argument('--delay', type=int, default=0, help='How many epochs to wait before training with constraints.')
+parser.add_argument('--print-freq', type=int, default=10, help='Print frequency.')
+parser.add_argument('--report-dir', type=str, required=True, help='Directory where results should be stored')
+parser.add_argument('--constraint', type=str, required=True, help='the constraint to train with: LipschitzT(L), LipschitzG(eps, L), RobustnessT(eps1, eps2), RobustnessG(eps, delta), CSimiliarityT(), CSimilarityG(), LineSegmentG()')
+parser.add_argument('--embed-dim', type=int, default=40, help='embed dim')
+parser.add_argument('--network-output', type=str, choices=['logits', 'prob', 'logprob'], default='logits', help='Wether to treat the output of the network as logits, probabilities or log(probabilities) in the constraints.')
+args = parser.parse_args()
+
+torch.manual_seed(42)
+np.random.seed(42)
+
+device = torch.device("cuda" if use_cuda else "cpu")
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+pca = None
+
+if args.dataset == 'mnist':
+    mnist_data = datasets.MNIST('../../data/mnist/', train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+
+    train_loader = torch.utils.data.DataLoader(mnist_data, shuffle=True, batch_size=args.batch_size, **kwargs)
+    test_loader = torch.utils.data.DataLoader(datasets.MNIST('../../data/mnist/', train=False, transform=transforms.Compose([
+        transforms.ToTensor()])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+
+    if args.embed:
+        pca = embed_pca(mnist_data, args.embed_dim)
+        model = MLP(args.embed_dim, 10, 1000, 3).to(device)
+    else:
+        model = MnistNet().to(device)
+
+elif args.dataset == 'fashion':
+    fashion_data = datasets.FashionMNIST('../../data/fashionmnist/', train=True, download=True,
+                                         transform=transforms.Compose([transforms.ToTensor()]))
+
+    train_loader = torch.utils.data.DataLoader(fashion_data, shuffle=True, batch_size=args.batch_size, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.FashionMNIST('../../data/fashionmnist/', train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+        ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+
+    if args.embed:
+        pca = embed_pca(fashion_data, args.embed_dim)
+        model = MLP(args.embed_dim, 10, 1000, 3).to(device)
+    else:
+        model = MnistNet().to(device)
+elif args.dataset == 'cifar10':
+    transform = transforms.Compose([transforms.ToTensor()])
+
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ])  # meanstd transformation
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    cifar_data = datasets.CIFAR10('../../data/cifar10', train=True, download=True, transform=transform_train)
+
+    train_loader = torch.utils.data.DataLoader(cifar_data, shuffle=True, batch_size=args.batch_size, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('../../data/cifar10', train=False, download=True, transform=transform_test),
+        batch_size=256, shuffle=True, **kwargs)
+
+    if args.embed:
+        pca = embed_pca(cifar_data, args.embed_dim)
+        model = MLP(args.embed_dim, 10, 1000, 3).to(device)
+    else:
+        model = ResNet50().to(device)
+
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+def RobustnessT(eps1, eps2):
+    return lambda model, use_cuda, network_output: RobustnessDatasetConstraint(model, eps1, eps2, use_cuda=use_cuda, network_output=network_output)
+
+def RobustnessG(eps, delta):
+    return lambda model, use_cuda, network_output: RobustnessConstraint(model, eps, delta, use_cuda, network_output=network_output)
+
+def LipschitzT(L):
+    return lambda model, use_cuda, network_output: LipschitzDatasetConstraint(model, L, use_cuda, network_output=network_output)
+
+def LipschitzG(eps, L):
+    return lambda model, use_cuda, network_output: LipschitzConstraint(model, eps=eps, l=L, use_cuda=use_cuda, network_output=network_output)
+
+def CSimilarityT(delta):
+    return lambda model, use_cuda, network_output: CifarDatasetConstraint(model, delta, use_cuda, network_output=network_output)
+
+def CSimilarityG(eps, delta):
+    return lambda model, use_cuda, network_output: CifarConstraint(model, eps, delta, use_cuda, network_output=network_output)
+
+def SegmentG(eps, delta):
+    return lambda model, use_cuda, network_output: PairLineRobustnessConstraint(model, eps, delta, use_cuda, network_output=network_output)
+
+constraint = eval(args.constraint)(model, use_cuda, network_output=args.network_output)
+oracle = DL2_Oracle(learning_rate=0.01, net=model, constraint=constraint, use_cuda=use_cuda)
+
+opt_type = 'T' if constraint.n_gvars == 0 else 'G'
+report_dir = os.path.dirname(
+    os.path.join(args.report_dir, '%s/%s/%s' % (opt_type, args.dataset, constraint.name)))
+
+if not os.path.exists(report_dir):
+    os.makedirs(report_dir)
+
+tstamp = int(time.time())
+
+exptype = 'baseline' if args.dl2_weight < 1e-7 else 'dl2'
+report_file = os.path.join(report_dir, 'report_%s_%s_%d.json' % (constraint.name, exptype, tstamp))
+data_dict = {
+    'dl2_weight': args.dl2_weight,
+    'pretrained': args.pretrained,
+    'delay': args.delay,
+    'name': constraint.name,
+    'constraint_txt': args.constraint,
+    'constraint_params': constraint.params(),
+    'num_iters': args.num_iters,
+    'train_acc': [],
+    'constr_acc': [],
+    'dl2_loss': [],
+    'ce_loss': [],
+    'p_acc': [],
+    'c_acc': [],
+    'epoch_time': []
+}
+
+for epoch in range(1, args.num_epochs + 1):
+    avg_train_acc, avg_constr_acc, avg_dl2_loss, avg_ce_loss, epoch_time = \
+        train(args, oracle, model, device, train_loader, optimizer, epoch, pca)
+    data_dict['train_acc'].append(avg_train_acc)
+    data_dict['constr_acc'].append(avg_constr_acc)
+    data_dict['ce_loss'].append(avg_ce_loss)
+    data_dict['dl2_loss'].append(avg_dl2_loss)
+    data_dict['epoch_time'].append(epoch_time)
+
+    p, c = test(args, oracle, model, device, test_loader, pca)
+    data_dict['p_acc'].append(p)
+    data_dict['c_acc'].append(c)
+    print('Epoch Time [s]:', epoch_time)
+
+with open(report_file, 'w') as fou:
+    json.dump(data_dict, fou, indent=4)
