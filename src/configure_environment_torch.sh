20ddf0c685bb06f496e13355bb7a42b9ea8a26cb
HymnOfLight <30403502+HymnOfLight@users.noreply.github.com>
2022-11-21 00:19:40 +0000

    Update configure_environment_torch.sh

--- src/configure_environment_torch.sh
+++ src/configure_environment_torch.sh
@@ -1,7 +1,14 @@
 #configure torch
 
+echo "first installing anaconda"
+curl https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh --output anaconda.sh
+bash anaconda.sh
+
+echo "creating virtual environment for torch"
+echo "DeepRepair ART"
 conda create -n torch python
 conda activate torch
 conda install pytorch torchvision tqdm scipy scikit-learn matplotlib numpy
 pip install onnx
 pip install diffabs
+conda deactivate
